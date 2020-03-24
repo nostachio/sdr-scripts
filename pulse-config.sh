732c40f16f402b874e1a9c6a7f96d9178b87e1ed
Administrator <adam.silkroski@gmail.com>
2020-03-24 15:38:48 -0700

    Add descriptions for debugging.

--- pulse-config.sh
+++ pulse-config.sh
@@ -8,12 +8,17 @@ RADIO="alsa_input.usb-Burr-Brown_from_TI_USB_Audio_CODEC-00.analog-stereo"
 #set sink name
 SINK="dummy"
 #get sound from rig source
+echo "Setting default souce to radio source"
 pacmd set-default-source ${RADIO}
 #create an empty sink
+echo "Create empty sink"
 pactl load-module module-null-sink sink_name=${SINK}
 #make that sink the default
+echo "Making sink default"
 pacmd set-default-sink ${SINK}
 #mirror radio source to the sink
+echo "Mirroring radio source to sink"
 pacmd load-module module-loopback source=${RADIO} sink=${SINK}
 #mirror nx (aka nomachine) server input to rig
+echo "Mirroring NX (aka NoMachine) source to sink"
 pactl load-module module-loopback source=nx_voice_out.monitor sink=${RADIO}
