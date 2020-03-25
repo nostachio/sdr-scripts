8ab38e6d4d64168628d493321f8583ebd2010853
Administrator <adam.silkroski@gmail.com>
2020-03-25 14:52:15 -0700

    Use null sink monitor.

--- pulse-config.sh
+++ pulse-config.sh
@@ -21,24 +21,20 @@ then
   exit 2
 fi
 
-#check if NoMachine sources are present (if not, there is not a current NoMachine session and the scripts shouldn't change anything.) or exit with message
-pacmd list-sources | grep name: | grep 'nx_voice_out.monitor' >/dev/null
+#check if NoMachine sources are present (if not, there is not a current NoMachine session and the scripts shouldn't change anything.) or exit with message to reconnect with NoMachine to repopulate its pulseaudio sources and sinks.
+pacmd list-sources | grep name: | grep 'nx_voice_out' >/dev/null
 if [ $? -ne 0 ]
 then
-  echo "NoMachine sources not available in Pulseaudio.  This script should only be run while connected via NoMachine.  If you are connected via NoMachine, please check the audio settings and try again."
-  echo "You may need to restart the nxserver or update the NoMachine audio support."
-  echo "Restart the nxserver via:"
-  echo "sudo ${NXPATH}/bin/nxserver --restart"
-  echo "Update NoMachine audio support via:"
-  echo "sudo ${NXPATH}/bin/nxnode --audiosetup"
+  echo "NoMachine sink not available in Pulseaudio.  This script should only be run while connected via NoMachine.  If you are connected via NoMachine, please check the audio settings and try again."
+  echo "You may need to terminate your NoMachine session and then reconnect."
   exit 3
 fi
 
 #detect sources and sinks
 #from pi
 PI_IN_SOURCE=$(pacmd list-sources | grep platform | tr -d '<>' | awk '{print $2}')
-# to pi
-PI_OUT_SINK=$(pacmd list-sinks | grep platform | tr -d '<>' | awk '{print $2}')
+# to pi (unused, but may be handy at some point)
+#PI_OUT_SINK=$(pacmd list-sinks | grep platform | tr -d '<>' | awk '{print $2}')
 # from rig
 RADIO_IN=$(pacmd list-sources | grep name: | grep input | tr -d '<>' | awk '{print $2}')
 # to rig
@@ -47,11 +43,8 @@ RADIO_OUT_SINK=$(pacmd list-sinks | grep name: | grep usb | grep output | tr -d 
 NOMACHINE_IN=$(pacmd list-sources | grep name: | grep nx | grep monitor | tr -d '<>' | awk '{print $2}')
 #to remote nomachine
 NOMACHINE_OUT_SINK=$(pacmd list-sinks | grep name: |grep nx_voice_out | tr -d '<>' | awk '{print $2}')
-#create sinks and loopbacks
 
-#get sound from rig source
-# echo "Setting default souce to radio source"
-# pacmd set-default-source ${COMBINED_SINK}
+#create sinks and loopbacks
 #create an empty sink if not already present (if present pulseaudio gives a 53 error)
 pacmd list-sinks |grep "name: <${COMBINED_SINK}>"
 if [ $? -eq 0 ]
@@ -61,18 +54,13 @@ else
   echo "Creating sink ${COMBINED_SINK}"
   pactl load-module module-null-sink sink_name=${COMBINED_SINK}
 fi
-#make that sink the default
-# echo "Making sink default"
-# pacmd set-default-sink ${SINK}
-#mirror pi output to remote nomachine
-#mirror radio source to the sink
-#mirror pi to remote NoMachine
+
 # Combine pi and radio into one sink
+echo "Sending system audio to combined sink."
 pacmd load-module module-loopback source=${PI_IN_SOURCE} sink=${COMBINED_SINK}
-echo "Mirroring radio source to sink"
+echo "Sending radio audio input to combined sink."
 pacmd load-module module-loopback source=${RADIO_IN} sink=${COMBINED_SINK}
-#send combined sink to remote NoMachine
+echo "Sending combined sink monitor to remote NoMachine."
 pacmd load-module module-loopback source=${COMBINED_SINK}.monitor sink=${NOMACHINE_OUT_SINK}
-#mirror nx (aka nomachine) server input to rig
-echo "Mirroring NX (aka NoMachine) source to sink"
+echo "Mirroring remote NoMachine audio to radio."
 pactl load-module module-loopback source=${NOMACHINE_IN} sink=${RADIO_OUT_SINK}
