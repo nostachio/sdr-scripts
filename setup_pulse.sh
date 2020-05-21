f27d43f58ad745b12315c136cab4839de941dc9a
Administrator <adam.silkroski@gmail.com>
2020-05-21 11:16:04 -0700

    Make close terminal better.

--- setup_pulse.sh
+++ setup_pulse.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 pulseaudio --start
-lxterminal -e "/home/pi/sdr-scripts/pulse-config.sh; echo 'Press any key to close window.'; read"
+lxterminal -e "/home/pi/sdr-scripts/pulse-config.sh; echo 'Press enter to close window.'; read"
